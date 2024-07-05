e4db2d54c21d22a28c185867b478fc2064d75ffb
abnerolimor <abner.oliveira.moraes@escola.pr.gov.br>
2024-07-05 11:17:33 -0300

    Add files via upload

---
+++ Abner/qualquercoisa_scrip.sql
@@ -0,0 +1,49 @@
+create database qualquercoisa;
+
+use qualquercoisa;
+
+create table funcionarios(
+pk int not null auto_increment, primary key(pk),
+nome varchar(255) not null,
+login varchar(255) not null
+);
+
+create table produto(
+pk int not null auto_increment, primary key(pk),
+nome varchar(255) not null,
+valor int not null
+);
+
+create table venda(
+	pk int not null auto_increment, primary key(pk),
+	produto varchar(255) not null,
+	operador varchar(255) not null,
+	data_venda date not null,
+	horario date not null,
+	funcionarios_fk int not null, foreign key(funcionarios_fk) references funcionarios(pk),
+	produto_fk int not null, foreign key(produto_fk) references produto(pk) 
+    );
+    
+    create table pix (
+		pk int not null auto_increment, primary key(pk),
+		aluno_nome varchar(255) not null,
+        aluno_turma varchar(255) not null,
+        venda_fk int not null, foreign key( venda_fk) references  venda(pk) 
+    );
+    
+    create table caixa (
+		pk int not null auto_increment, primary key(pk),
+        nome varchar(255) not null,
+        fundo_troco int not null,
+        sangria_troco int not null,
+        reforco varchar(255) not null,
+		venda_fk int not null, foreign key( venda_fk) references  venda(pk) 
+        );
+        
+        create table historico_venda (
+			pk int not null auto_increment, primary key(pk),
+            data_venda date not null,
+            operador varchar(255) not null,
+            valor int not null,
+            venda_fk int not null, foreign key( venda_fk) references  venda(pk)
+        );
